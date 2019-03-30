2d75e92eb23bc6a69eb24baee14ec3f38c547578
Phoenix314159 <derivative7117@live.com>
2019-03-30 11:50:11 -0600

    addedd COmmentList Component

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,5 +1,27 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
 
-export default () => {
-    return <div>This is the comment list</div>
+class CommentList extends Component {
+
+    renderComments() {
+        const {comments} = this.props
+        return comments.map(comment => {
+            return <li key={comment}>{comment}</li>
+        })
+    }
+
+    render() {
+
+        return(
+            <div>
+                <ul>
+                    {this.renderComments()}
+                </ul>
+            </div>
+        )
+    }
 }
+
+const mapStateToProps = ({comments}) => ({comments})
+
+export default connect(mapStateToProps)(CommentList)
--- src/reducers/commentsReducer.js
+++ src/reducers/commentsReducer.js
@@ -4,6 +4,7 @@ import {SAVE_COMMENT} from 'actions/types'
 export default (state = [], action) => {
     const {type, payload} = action;
 
+
     switch (type) {
         case SAVE_COMMENT:
             return [...state, payload];
